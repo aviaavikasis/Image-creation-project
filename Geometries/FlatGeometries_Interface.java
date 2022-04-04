93b45fca9bde28b082fbea22131b3c00de80cb2a
Avia Avikasis <62906801+aviaavikasis@users.noreply.github.com>
2022-04-04 22:21:08 +0300

    Create FlatGeometries_Interface.java

---
+++ Geometries/FlatGeometries_Interface.java
@@ -0,0 +1,8 @@
+package geometries;
+
+/**
+ * The FlatGeometries class is a title for marking flat geometries that have no volume
+ * @author : Avia Avikasis
+ */
+public interface FlatGeometries {
+}
