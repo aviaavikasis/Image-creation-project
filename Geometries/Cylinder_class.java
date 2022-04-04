eb0eedf34f7c1cc0167de7c0682245c949a6d919
Avia Avikasis <62906801+aviaavikasis@users.noreply.github.com>
2022-04-04 22:17:24 +0300

    Update Cylinder_class.java

--- Geometries/Cylinder_class.java
+++ Geometries/Cylinder_class.java
@@ -1,8 +1,35 @@
 package geometries;
+import Primitives.*;
 
-public class Cylinder extends RadialGeometry {
+import java.util.ArrayList;
+
+/**
+ *The cylinder class will represent a point and height cylinder (length)
+ * The class inherits from the RadialGeometry interface
+ * @author : Avia Avikasis
+ */
+public  class Cylinder extends RadialGeometry
+{
+    Point3D Point;
     double High;
 
-    public Cylinder() {
+
+
+    // A constructor that receives a Point3D point and height, and returns a cylinder with the data it receives
+
+    public Cylinder(Point3D p, double H)
+    {
+        Point = p;
+        High = H;
     }
+
+    //Executes the getNormal function that returns the normal vector to the body
+    public Vector getNormal(Point3D p)
+    {
+        Vector V = p.subtract(Point);
+        return V.normalized();
+    }
+
+    public ArrayList<GeoPoint> findIntersections(Ray ray){return null;}
+
 }
