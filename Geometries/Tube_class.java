cf50a3570f89aad431338e3a60d6bcc0f2feb174
Avia Avikasis <62906801+aviaavikasis@users.noreply.github.com>
2022-04-04 22:35:17 +0300

    Create Tube_class.java

---
+++ Geometries/Tube_class.java
@@ -0,0 +1,12 @@
+package geometries;
+import Primitives.*;
+
+import java.util.ArrayList;
+
+public class Tube extends RadialGeometry
+{
+    Ray Ra;
+    public  Vector getNormal(Point3D p){return  null;}
+  public ArrayList<GeoPoint> findIntersections(Ray ray){return  null;}
+
+}
