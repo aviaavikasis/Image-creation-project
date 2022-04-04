9b6ff615e8d8fe8d20377bd7dd684b3f10d86772
Avia Avikasis <62906801+aviaavikasis@users.noreply.github.com>
2022-04-04 22:44:23 +0300

    Update Ray_class.java

--- primitives/Ray_class.java
+++ primitives/Ray_class.java
@@ -1,9 +1,32 @@
-package primitives;
+package Primitives;
 
-public class Ray {
+/**
+ * The Ray class represents a beam that exits from a point in space with a direction vector
+ * @author : Avia Avikasis
+ */
+public class Ray
+{
+    Point3D P;
+    Vector V;
     Coordinate Zero;
     Coordinate A;
 
-    public Ray() {
+////////////////////////////////////////////////
+
+    //ctor:
+    public Ray(Point3D p,Vector v)
+    {
+        P = p;
+        V = v;
+    }
+
+    public Point3D getp()
+    {
+        return P;
+    }
+
+    public Vector getv()
+    {
+        return V;
     }
 }
